610801e1881e7d19c8a004eb71d85e85ca88d79d
caitao <15624951815@163.com>
2018-03-29 10:17:28 +0800

    extend dataset

--- main.py
+++ main.py
@@ -9,49 +9,13 @@ import os
 from PIL import Image
 import re
 import numpy as np
-import time
+from torch.utils.data import Dataset
 
 
 MAX_D = 192
 root_path = "/home/caitao/Downloads/tmp_data/data/a_rain_of_stones_x2"
 truth_path = "/home/caitao/Downloads/tmp_data/groundtruth/a_rain_of_stones_x2/left"
 cuda_available = False
-
-
-def conv5x5(in_channels, out_channels):
-    return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size=5,
-                  stride=2, padding=1),
-        nn.BatchNorm2d(out_channels),
-        nn.ReLU()
-    )
-
-
-def conv3x3x3(in_channels, out_channels):
-    return nn.Sequential(
-        nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=2),
-        nn.BatchNorm3d(out_channels),
-        nn.ReLU()
-    )
-
-
-def conv3x3x3_padding(in_channels, out_channels):
-    return nn.Sequential(
-        nn.Conv3d(in_channels, out_channels,
-                  kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm3d(out_channels),
-        nn.ReLU()
-    )
-
-
-def cost_volume_generation(l, r, max_disparity):
-    ans = []
-    ans.append(torch.cat([l, r], dim=1))
-    for t in range(1, max_disparity + 1):
-
-        ans.append(
-            torch.cat([l, torch.cat([r[..., t:], r[..., :t]], dim=3)], dim=1))
-    return torch.stack(ans, dim=4)
 
 
 def readPFM(file):
@@ -92,38 +56,97 @@ def readPFM(file):
     return data, scale
 
 
-def get_data():
-    def get_one(child_dir):
-        data_dir = os.path.join(root_path, child_dir)
-        images_list = os.listdir(data_dir)[:3]
-        for i, filename in enumerate(images_list):
-            image_path = os.path.join(data_dir, filename)
-            image = Image.open(image_path)
-            transform_list = [transforms.ToTensor(),
-                              transforms.Normalize((0.5, 0.5, 0.5),
-                                                   (0.5, 0.5, 0.5))]
-            transform = transforms.Compose(transform_list)
-            image = transform(image)
-            if i == 0:
-                res = torch.unsqueeze(image, dim=0)
-            else:
-                res = torch.cat([res, torch.unsqueeze(image, dim=0)], dim=0)
-        return res, images_list
-    l, imglist = get_one("left")
-    r, imglist_r = get_one("right")
-    assert imglist == imglist_r
+class MyDataset(Dataset):
+    def __init__(self, root_path, truth_path, transform=None, target_transform=None):
+        super(MyDataset, self).__init__()
+        self.root_path = root_path
+        self.truth_path = truth_path
+        self.transform = transform
+        self.target_transform = target_transform
+        self.data = self._make_dataset()
 
-    truth_namelist = [x[:x.rindex('.') + 1] + "pfm" for x in imglist]
-    print(truth_namelist)
+    def __getitem__(self, index):
+        if index < 0 or index >= self.__len__():
+            raise(Exception("index out of range"))
+        l = self.data[0]
+        r = self.data[1]
+        truth = self.data[2]
+        return l[index:index+1], r[index:index+1], truth[index:index+1]
+
+    def __len__(self):
+        return len(self.data)
+
+    def _make_dataset(self):
+        def get_one(child_dir):
+            data_dir = os.path.join(self.root_path, child_dir)
+            images_list = os.listdir(data_dir)
+            for i, filename in enumerate(images_list):
+                image_path = os.path.join(data_dir, filename)
+                image = Image.open(image_path)
+                transform = self.transform
+                image = transform(image)
+                if i == 0:
+                    res = torch.unsqueeze(image, dim=0)
+                else:
+                    res = torch.cat(
+                        [res, torch.unsqueeze(image, dim=0)], dim=0)
+            return res, images_list
+        l, imglist = get_one("left")
+        r, imglist_r = get_one("right")
+        assert imglist == imglist_r
+
+        truth_namelist = [x[:x.rindex('.') + 1] + "pfm" for x in imglist]
+        print(truth_namelist)
+        ans = []
+        for ii, truth_name in enumerate(truth_namelist):
+            abso_path = os.path.join(self.truth_path, truth_name)
+            onetruth, _ = readPFM(abso_path)
+            onetruth = torch.FloatTensor(onetruth.tolist())
+            ans.append(onetruth)
+
+        truth = torch.stack(ans, dim=0)
+        return l, r, truth
+
+
+train_loader = torch.utils.data.DataLoader(MyDataset(root_path, truth_path, transform=transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])), batch_size=1)
+
+
+def conv5x5(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=5,
+                  stride=2, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU()
+    )
+
+
+def conv3x3x3(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=2),
+        nn.BatchNorm3d(out_channels),
+        nn.ReLU()
+    )
+
+
+def conv3x3x3_padding(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv3d(in_channels, out_channels,
+                  kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm3d(out_channels),
+        nn.ReLU()
+    )
+
+
+def cost_volume_generation(l, r, max_disparity):
     ans = []
-    for ii, truth_name in enumerate(truth_namelist):
-        abso_path = os.path.join(truth_path, truth_name)
-        onetruth, _ = readPFM(abso_path)
-        onetruth = torch.FloatTensor(onetruth.tolist())
-        ans.append(onetruth)
-
-    truth = torch.stack(ans, dim=0)
-    return l[0:1, ...], r[0:1, ...], truth[0:1, ...]
+    ans.append(torch.cat([l, r], dim=1))
+    for t in range(1, max_disparity + 1):
+        ans.append(
+            torch.cat([l, torch.cat([r[..., t:], r[..., :t]], dim=3)], dim=1))
+    return torch.stack(ans, dim=4)
 
 
 # Residual Block
@@ -203,72 +226,74 @@ class GCNet(nn.Module):
         # )
 
     def forward(self, l, r):
-        with torch.cuda.device(0):
-            l = self.conv1(l)
-            l = self.block8(self.block7(self.block6(self.block5(
-                self.block4(self.block3(self.block2(self.block1(l))))))))
-            l = self.conv2(l)
+        l = self.conv1(l)
+        l = self.block8(self.block7(self.block6(self.block5(
+            self.block4(self.block3(self.block2(self.block1(l))))))))
+        l = self.conv2(l)
 
-            r = self.conv1(r)
-            r = self.block8(self.block7(self.block6(self.block5(
-                self.block4(self.block3(self.block2(self.block1(r))))))))
-            r = self.conv2(r)
+        r = self.conv1(r)
+        r = self.block8(self.block7(self.block6(self.block5(
+            self.block4(self.block3(self.block2(self.block1(r))))))))
+        r = self.conv2(r)
 
-        with torch.cuda.device(1):
-            # v = cost_volume_generation(l, r, 46)
-            v = cost_volume_generation(l, r, 95)
+        # v = cost_volume_generation(l, r, 46)
+        v = cost_volume_generation(l, r, 95)
 
-            out21 = self.conv21(v)
-            out24 = self.conv24(out21)
-            out27 = self.conv27(out24)
+        out21 = self.conv21(v)
+        out24 = self.conv24(out21)
+        out27 = self.conv27(out24)
 
-            out = self.conv29(self.conv28(out27)) + \
-                self.deconv33(self.conv32(self.conv31(self.conv30(out27))))
-        with torch.cuda.device(2):
-            out = self.conv26(self.conv25(out24)) + self.deconv34(out)
+        out = self.conv29(self.conv28(out27)) + \
+            self.deconv33(self.conv32(self.conv31(self.conv30(out27))))
+        out = self.conv26(self.conv25(out24)) + self.deconv34(out)
 
-            out = self.conv23(self.conv22(out21)) + self.deconv35(out)
+        out = self.conv23(self.conv22(out21)) + self.deconv35(out)
 
-            out = self.conv20(self.conv19(v)) + self.deconv36(out)
+        out = self.conv20(self.conv19(v)) + self.deconv36(out)
 
-            out = self.deconv37(out)
+        out = self.deconv37(out)
 
-            out = (nn.Softmax(dim=4))(torch.mul(out, -1))
-            res = []
-            for i in range(out.size()[2]):
-                if len(res) == 0:
-                    res.append(torch.mul(out[:, :, :, :, i], i))
-                else:
-                    res[0] += torch.mul(out[:, :, :, :, i], i)
-            out = torch.squeeze(res[0], dim=1)
+        out = (nn.Softmax(dim=4))(torch.mul(out, -1))
+        res = []
+        for i in range(out.size()[2]):
+            if len(res) == 0:
+                res.append(torch.mul(out[:, :, :, :, i], i))
+            else:
+                res[0] += torch.mul(out[:, :, :, :, i], i)
+        out = torch.squeeze(res[0], dim=1)
         return out
 
 
-def train_gcnet():
-    start = time.time()
-    net = GCNet()
+net = GCNet()
+if torch.cuda.is_available():
+    global cuda_available
+    cuda_available = True
+    net.cuda()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.5)
-    if torch.cuda.device_count() > 1:
-        print("Use ", torch.cuda.device_count(), " GPUs!")
-        with torch.cuda.device(0):
-            net = nn.DataParallel(net, device_ids=[0])
 
-    if torch.cuda.is_available():
-        global cuda_available
-        cuda_available = True
 
-    with torch.cuda.device(0):
-        l, r, truth = get_data()
-        l, r, truth = Variable(l), Variable(r), Variable(truth)
+def train_gcnet(epoch):
+    net.train()
+
+    for batch_idx, (l, r, truth) in enumerate(train_loader):
         optimizer.zero_grad()
         out = net(l, r)
 
         loss = F.l1_loss(out, truth)
         loss.backward()
         optimizer.step()
-    print("successfully train once!")
-    end = time.time()
-    print(end - start)
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(truth), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
 
 
-train_gcnet()
+def test_gcnet():
+    pass
+
+
+epochs = 1
+
+for epoch in range(1, epochs + 1):
+    train_gcnet(epoch)
+    test_gcnet()
